2c9f7f576c223a3597f9dcb43b37dce6b8ff7deb
felix.winangun <fwlix.winangun@kawanlamaretail.com>
2023-07-18 11:14:55 +0700

    fix: sold out param

--- Dto/SoldOut/SoldOutParam.cs
+++ Dto/SoldOut/SoldOutParam.cs
@@ -4,8 +4,7 @@
     {
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string SiteCode { get; set; }
-        public string Sku { get; set; }
+        public string Search { get; set; }
         public bool? IsGlobal { get; set; }
         public int Size { get; set; }
         public int Page { get; set; }
